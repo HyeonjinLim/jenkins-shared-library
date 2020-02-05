e4a0141104fe14a0253a08d26e1ec6af407c470c
HyeonjinLim <hyeonjin.lim@stradvision.com>
2020-02-05 15:48:56 +0900

    Added Jenkins Slack Notification Function

--- vars/Notification.groovy
+++ vars/Notification.groovy
@@ -1,25 +1,34 @@
-def call(String buildStatus) {
-  // build status of null means successful
-  //buildStatus =  buildStatus ?: 'SUCCESSFUL'
+blocks = [
+		{
+			"type": "section",
+			"text": {
+				"type": "mrkdwn",
+				"text": "<http://13.124.166.12:8080|QAC Board>"
+			}
+		}
+	]
 
+def call(String buildStatus) {
   // Default values
   def colorName = 'RED'
   def colorCode = '#FF0000'
-  def subject = "${buildStatus}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'"
+  def subject = "#${env.BUILD_NUMBER} ${buildStatus}: QAC analysis Success. You can check the results in QAC Dashboard http://13.124.166.12:8080."
   def summary = "${subject} (${env.BUILD_URL})"
 
   // Override default values based on build status
   if (buildStatus == 'UNSTABLE') {
     color = 'YELLOW'
     colorCode = '#FFFF00'
-  } else if (buildStatus == 'SUCCESSFUL') {
+    subject = "#${env.BUILD_NUMBER} ${buildStatus}: Analysis Unstable. You should be check Jenkins."
+  } else if (buildStatus == 'SUCCESS') {
     color = 'GREEN'
     colorCode = '#00FF00'
   } else {
     color = 'RED'
     colorCode = '#FF0000'
+    subject = "#${env.BUILD_NUMBER} ${buildStatus}: Analysis Failed. You should be check Jenkins."
   }
 
   // Send notifications
-  slackSend (color: colorCode, message: summary)
+  slackSend (color: colorCode, message: summary, blocks=blocks)
 }
